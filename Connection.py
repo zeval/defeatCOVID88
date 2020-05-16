998f4ffda5f00b1ca8899e88b90b88707114b64b
Augusto Gouveia <estacontapertence@gmail.com>
2020-05-16 16:56:48 +0100

    Convert Connection into a WeightedEdge

--- Connection.py
+++ Connection.py
@@ -5,6 +5,8 @@ class Connection(object):
         """
         self.src = src
         self.dest = dest
+        hoursInADay = 24
+        self.weight = (dest.getFitness() / src.getAge()) * hoursInADay
 
     def getSource(self):
         return self.src
@@ -12,8 +14,12 @@ class Connection(object):
     def getDestination(self):
         return self.dest
 
+    def getWeight(self):
+        return self.weight
+
     def __str__(self):
-        return self.src.getName() + '->' + self.dest.getName()
+        return self.getSource().getName() + '->(' + str(self.weight) + ')' \
+               + self.getDestination().getName()
 
     # when handed a Connection object, SocialNetwork must add it reciprocally
     # between source and destination given that we're working with a graph.
